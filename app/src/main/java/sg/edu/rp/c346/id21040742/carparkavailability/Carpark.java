47199da075a70656cca16c54d4eefa7df1cedd14
fikry <21040742@myrp.edu.sg>
2022-08-10 14:35:18 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21040742/carparkavailability/Carpark.java
@@ -0,0 +1,52 @@
+package sg.edu.rp.c346.id21040742.carparkavailability;
+
+public class Carpark {
+    private String total_lots;
+    private String lot_type;
+    private String lots_available;
+    private String carpark_number;
+
+    public Carpark(String total_lots, String lot_type, String lots_available,String carpark_number) {
+        this.total_lots = total_lots;
+        this.lot_type = lot_type;
+        this.lots_available = lots_available;
+        this.carpark_number = carpark_number;
+    }
+
+    public String getCarpark_number() {
+        return carpark_number;
+    }
+
+    public void setCarpark_number(String carpark_number) {
+        this.carpark_number = carpark_number;
+    }
+
+    public String getTotal_lots() {
+        return total_lots;
+    }
+
+    public void setTotal_lots(String total_lots) {
+        this.total_lots = total_lots;
+    }
+
+    public String getLot_type() {
+        return lot_type;
+    }
+
+    public void setLot_type(String lot_type) {
+        this.lot_type = lot_type;
+    }
+
+    public String getLots_available() {
+        return lots_available;
+    }
+
+    public void setLots_available(String lots_available) {
+        this.lots_available = lots_available;
+    }
+
+    public String toString(){
+        String msg = "Carpark Number: " + getCarpark_number() + "\nLot Type: " + getLot_type() + "\nLots Available: " + getLots_available() +"\nTotal Lots: " + getTotal_lots();
+        return msg;
+    }
+}
---
+++ app/src/main/java/sg/edu/rp/c346/id21040742/carparkavailability/MainActivity.java
@@ -0,0 +1,93 @@
+package sg.edu.rp.c346.id21040742.carparkavailability;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvCarpark;
+    AsyncHttpClient client;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvCarpark = findViewById(R.id.lvCarpark);
+        client = new AsyncHttpClient();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        ArrayList<Carpark> alCarpark = new ArrayList<Carpark>();
+
+
+        client.get("https://api.data.gov.sg/v1/transport/carpark-availability", new JsonHttpResponseHandler() {
+
+            String total_lots;
+            String lot_type;
+            String lots_available;
+            String carpark_number;
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+                try {
+                    JSONArray jsonArrItems = response.getJSONArray("items");
+                    JSONObject firstObj = jsonArrItems.getJSONObject(0);
+                    JSONArray jsonArrCarparkData = firstObj.getJSONArray("carpark_data");
+
+
+                    for (int i = 0; i < jsonArrCarparkData.length(); i++) {
+                        JSONObject jsonObjCarparkData = jsonArrCarparkData.getJSONObject(i);
+                        JSONArray jsonArrCarparkInfo = jsonObjCarparkData.getJSONArray("carpark_info");
+                        JSONObject jsonObjCarparkData2 = jsonArrCarparkInfo.getJSONObject(0);
+
+                        carpark_number = jsonObjCarparkData.getString("carpark_number");
+                        total_lots = jsonObjCarparkData2.getString("total_lots");
+                        lot_type = jsonObjCarparkData2.getString("lot_type");
+                        lots_available = jsonObjCarparkData2.getString("lots_available");
+
+                        Carpark carpark = new Carpark(total_lots, lot_type, lots_available,carpark_number);
+                        alCarpark.add(carpark);
+                    }
+                } catch (JSONException e) {
+                    Log.d("Exception", e.toString());
+
+                }
+
+                //POINT X – Code to display List View
+
+                ArrayAdapter<Carpark> adapter = new ArrayAdapter<Carpark>(MainActivity.this,android.R.layout.simple_list_item_1,alCarpark);
+                lvCarpark.setAdapter((adapter));
+
+
+            }//end onSuccess
+        });
+    }//end onResume
+}
